6a573c0f538f8fffa37748fb9dc8c102022ae9cd
Shah Hassan <shahhassancs@gmail.com>
2024-05-22 12:26:29 +0500

    Update ajax.service.js

--- src/ajax.service.js
+++ src/ajax.service.js
@@ -18,6 +18,7 @@ const AjaxCall = (type, api, params, is_cache, is_auth) => {
     //set api
     // var url = "https://ec2-54-211-21-110.compute-1.amazonaws.com/"
     // var url = "https://localhost:4000/" + api;
+
     var url = "https://139.59.77.50:4000/" + api;
 
     axios({
